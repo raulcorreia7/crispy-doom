f98948e5ccda71105eb131410ca137a456088652
Raul Correia <raulcorreia7@users.noreply.github.com>
2026-06-22 19:09:09 +0200

    feat(dmcp): build runtime with crispy doom

--- cmake/DMCPIntegration.cmake
+++ cmake/DMCPIntegration.cmake
@@ -2,6 +2,8 @@ set(CRISPY_DMCP_WINDOWS_IMPORT_LIBRARY "dmcp.lib")
 set(CRISPY_DMCP_WINDOWS_RUNTIME_LIBRARY "dmcp.dll")
 set(CRISPY_DMCP_LINUX_RUNTIME_LIBRARY "libdmcp.so")
 set(CRISPY_DMCP_MACOS_RUNTIME_LIBRARY "libdmcp.dylib")
+set(CRISPY_DMCP_RUNTIME_TARGET "crispy_dmcp_runtime")
+set(CRISPY_DMCP_RUNTIME_BUILD_TARGET "crispy_dmcp_runtime_build")
 
 function(crispy_dmcp_configure)
     if(NOT DMCP_ENABLE)
@@ -11,6 +13,9 @@ function(crispy_dmcp_configure)
     set(resolved_dmcp_root "${DMCP_ROOT}")
     if(NOT resolved_dmcp_root AND NOT "$ENV{dmcp_DIR}" STREQUAL "")
         set(resolved_dmcp_root "$ENV{dmcp_DIR}")
+    endif()
+    if(NOT resolved_dmcp_root AND DEFINED CRISPY_DMCP_ROOT)
+        set(resolved_dmcp_root "${CRISPY_DMCP_ROOT}")
     endif()
     if(NOT resolved_dmcp_root)
         message(FATAL_ERROR "DMCP_ENABLE=ON requires DMCP_ROOT to be set")
@@ -32,6 +37,9 @@ function(crispy_dmcp_configure)
     if(NOT resolved_build_dir AND NOT "$ENV{DMCP_BUILD_DIR}" STREQUAL "")
         set(resolved_build_dir "$ENV{DMCP_BUILD_DIR}")
     endif()
+    if(NOT resolved_build_dir AND DEFINED CRISPY_DMCP_BUILD_DIR)
+        set(resolved_build_dir "${CRISPY_DMCP_BUILD_DIR}")
+    endif()
     if(NOT resolved_build_dir)
         set(resolved_build_dir "${resolved_dmcp_root}/build/default")
     endif()
@@ -39,6 +47,9 @@ function(crispy_dmcp_configure)
     set(resolved_library_dir "${DMCP_LIBRARY_DIR}")
     if(NOT resolved_library_dir AND NOT "$ENV{DMCP_LIBRARY_DIR}" STREQUAL "")
         set(resolved_library_dir "$ENV{DMCP_LIBRARY_DIR}")
+    endif()
+    if(NOT resolved_library_dir)
+        set(resolved_library_dir "${resolved_build_dir}")
     endif()
 
     set(DMCP_ROOT "${resolved_dmcp_root}" CACHE PATH "Path to DMCP SDK root directory" FORCE)
@@ -57,6 +68,87 @@ function(crispy_dmcp_configure)
     message(STATUS "DMCP enabled: adapter at ${resolved_adapter_dir}")
     message(STATUS "DMCP frame capture: ${DMCP_FRAME_CAPTURE}")
     message(STATUS "DMCP build dir: ${resolved_build_dir}")
+
+    crispy_dmcp_expected_runtime(dmcp_link_library dmcp_runtime_library)
+    crispy_dmcp_declare_runtime("${dmcp_link_library}" "${dmcp_runtime_library}")
+    set_property(GLOBAL PROPERTY CRISPY_DMCP_RUNTIME_LIBRARY "${dmcp_runtime_library}")
+endfunction()
+
+function(crispy_dmcp_expected_runtime out_link_library out_runtime_library)
+    if(WIN32)
+        set(link_library "${DMCP_BUILD_DIR}/${CRISPY_DMCP_WINDOWS_IMPORT_LIBRARY}")
+        set(runtime_library "${DMCP_BUILD_DIR}/${CRISPY_DMCP_WINDOWS_RUNTIME_LIBRARY}")
+    elseif(APPLE)
+        set(link_library "${DMCP_BUILD_DIR}/${CRISPY_DMCP_MACOS_RUNTIME_LIBRARY}")
+        set(runtime_library "${link_library}")
+    else()
+        set(link_library "${DMCP_BUILD_DIR}/${CRISPY_DMCP_LINUX_RUNTIME_LIBRARY}")
+        set(runtime_library "${link_library}")
+    endif()
+
+    set(${out_link_library} "${link_library}" PARENT_SCOPE)
+    set(${out_runtime_library} "${runtime_library}" PARENT_SCOPE)
+endfunction()
+
+function(crispy_dmcp_declare_runtime link_library runtime_library)
+    if(NOT DMCP_ENABLE)
+        return()
+    endif()
+    if(TARGET ${CRISPY_DMCP_RUNTIME_TARGET})
+        return()
+    endif()
+
+    include(ExternalProject)
+
+    set(dmcp_build_type "${CMAKE_BUILD_TYPE}")
+    if(NOT dmcp_build_type)
+        set(dmcp_build_type "Release")
+    endif()
+
+    ExternalProject_Add(${CRISPY_DMCP_RUNTIME_BUILD_TARGET}
+        SOURCE_DIR "${DMCP_ROOT}"
+        BINARY_DIR "${DMCP_BUILD_DIR}"
+        CMAKE_ARGS
+            "-DDMCP_BUILD_SHARED=ON"
+            "-DDMCP_BUILD_SINGLE_DLL=ON"
+            "-DDMCP_BUILD_EXAMPLES=OFF"
+            "-DDMCP_BUILD_ADAPTER_FAKE=OFF"
+            "-DDMCP_BUILD_ADAPTER_ZDOOM=OFF"
+            "-DDMCP_BUILD_ADAPTER_CRISPY=OFF"
+            "-DCPM_SOURCE_CACHE=${DMCP_ROOT}/.cache"
+            "-DCMAKE_BUILD_TYPE=${dmcp_build_type}"
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${DMCP_BUILD_DIR}"
+            "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=${DMCP_BUILD_DIR}"
+            "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=${DMCP_BUILD_DIR}"
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG=${DMCP_BUILD_DIR}"
+            "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG=${DMCP_BUILD_DIR}"
+            "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG=${DMCP_BUILD_DIR}"
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE=${DMCP_BUILD_DIR}"
+            "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=${DMCP_BUILD_DIR}"
+            "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE=${DMCP_BUILD_DIR}"
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELWITHDEBINFO=${DMCP_BUILD_DIR}"
+            "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO=${DMCP_BUILD_DIR}"
+            "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELWITHDEBINFO=${DMCP_BUILD_DIR}"
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_MINSIZEREL=${DMCP_BUILD_DIR}"
+            "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL=${DMCP_BUILD_DIR}"
+            "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_MINSIZEREL=${DMCP_BUILD_DIR}"
+        BUILD_COMMAND "${CMAKE_COMMAND}" --build "${DMCP_BUILD_DIR}" --config "${dmcp_build_type}" --parallel
+        INSTALL_COMMAND ""
+        BUILD_BYPRODUCTS "${link_library}" "${runtime_library}"
+    )
+
+    if(WIN32)
+        add_library(${CRISPY_DMCP_RUNTIME_TARGET} SHARED IMPORTED GLOBAL)
+        set_target_properties(${CRISPY_DMCP_RUNTIME_TARGET} PROPERTIES
+            IMPORTED_IMPLIB "${link_library}"
+            IMPORTED_LOCATION "${runtime_library}")
+    else()
+        add_library(${CRISPY_DMCP_RUNTIME_TARGET} SHARED IMPORTED GLOBAL)
+        set_target_properties(${CRISPY_DMCP_RUNTIME_TARGET} PROPERTIES
+            IMPORTED_LOCATION "${link_library}")
+    endif()
+
+    add_dependencies(${CRISPY_DMCP_RUNTIME_TARGET} ${CRISPY_DMCP_RUNTIME_BUILD_TARGET})
 endfunction()
 
 function(crispy_dmcp_adapter_sources out_var)
@@ -98,6 +190,10 @@ function(crispy_dmcp_apply_to_doom target)
 
     crispy_dmcp_adapter_sources(DMCP_ADAPTER_SOURCES)
     target_sources(${target} PRIVATE ${DMCP_ADAPTER_SOURCES})
+
+    if(TARGET ${CRISPY_DMCP_RUNTIME_BUILD_TARGET})
+        add_dependencies(${target} ${CRISPY_DMCP_RUNTIME_BUILD_TARGET})
+    endif()
 endfunction()
 
 function(crispy_dmcp_link_libraries out_var)
@@ -107,76 +203,11 @@ function(crispy_dmcp_link_libraries out_var)
         return()
     endif()
 
-    set(dmcp_library_bases)
-    foreach(dmcp_base IN ITEMS
-        "${DMCP_LIBRARY_DIR}"
-        "${DMCP_BUILD_DIR}"
-        "${DMCP_ROOT}/build/default"
-        "${DMCP_ROOT}/build/shared"
-        "${DMCP_ROOT}/build/integration"
-        "${DMCP_ROOT}/build")
-        if(NOT "${dmcp_base}" STREQUAL "")
-            list(APPEND dmcp_library_bases "${dmcp_base}")
-        endif()
-    endforeach()
-    list(REMOVE_DUPLICATES dmcp_library_bases)
-
-    set(dmcp_library_dirs)
-    foreach(dmcp_base IN LISTS dmcp_library_bases)
-        list(APPEND dmcp_library_dirs
-            "${dmcp_base}"
-            "${dmcp_base}/Release"
-            "${dmcp_base}/RelWithDebInfo"
-            "${dmcp_base}/Debug"
-            "${dmcp_base}/MinSizeRel")
-    endforeach()
-    list(REMOVE_DUPLICATES dmcp_library_dirs)
-
-    set(dmcp_link_libraries)
-    set(dmcp_expected_libraries)
-
-    foreach(dmcp_library_dir IN LISTS dmcp_library_dirs)
-        if(WIN32)
-            set(dmcp_single_libraries
-                "${dmcp_library_dir}/${CRISPY_DMCP_WINDOWS_IMPORT_LIBRARY}")
-            set(dmcp_single_runtime
-                "${dmcp_library_dir}/${CRISPY_DMCP_WINDOWS_RUNTIME_LIBRARY}")
-        elseif(APPLE)
-            set(dmcp_single_libraries
-                "${dmcp_library_dir}/${CRISPY_DMCP_MACOS_RUNTIME_LIBRARY}")
-        else()
-            set(dmcp_single_libraries
-                "${dmcp_library_dir}/${CRISPY_DMCP_LINUX_RUNTIME_LIBRARY}")
-        endif()
-
-        list(APPEND dmcp_expected_libraries
-            ${dmcp_single_libraries}
-        )
-
-        foreach(dmcp_single_library IN LISTS dmcp_single_libraries)
-            if(EXISTS "${dmcp_single_library}" AND (NOT WIN32 OR EXISTS "${dmcp_single_runtime}"))
-                set(dmcp_link_libraries "${dmcp_single_library}")
-                if(WIN32)
-                    set(dmcp_runtime_library "${dmcp_single_runtime}")
-                else()
-                    set(dmcp_runtime_library "${dmcp_single_library}")
-                endif()
-                break()
-            endif()
-        endforeach()
-        if(dmcp_link_libraries)
-            break()
-        endif()
-    endforeach()
-
-    if(NOT dmcp_link_libraries)
-        string(REPLACE ";" ", " expected_libraries "${dmcp_expected_libraries}")
-        message(FATAL_ERROR
-            "single shared DMCP runtime library not found. Expected one of: ${expected_libraries}")
+    if(NOT TARGET ${CRISPY_DMCP_RUNTIME_TARGET})
+        message(FATAL_ERROR "DMCP runtime target has not been declared")
     endif()
 
-    list(APPEND link_libraries ${dmcp_link_libraries})
-    set_property(GLOBAL PROPERTY CRISPY_DMCP_RUNTIME_LIBRARY "${dmcp_runtime_library}")
+    list(APPEND link_libraries ${CRISPY_DMCP_RUNTIME_TARGET})
     set(${out_var} "${link_libraries}" PARENT_SCOPE)
 endfunction()
 
@@ -188,6 +219,10 @@ function(crispy_dmcp_bundle_runtime target)
     get_property(dmcp_runtime_library GLOBAL PROPERTY CRISPY_DMCP_RUNTIME_LIBRARY)
     if(NOT dmcp_runtime_library)
         message(FATAL_ERROR "DMCP runtime library has not been resolved")
+    endif()
+
+    if(TARGET ${CRISPY_DMCP_RUNTIME_BUILD_TARGET})
+        add_dependencies(${target} ${CRISPY_DMCP_RUNTIME_BUILD_TARGET})
     endif()
 
     add_custom_command(TARGET ${target} POST_BUILD
